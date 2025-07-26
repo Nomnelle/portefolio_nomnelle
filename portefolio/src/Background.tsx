ee4df6153f52835aef982da1642375c4a02c9dd1
Nomnelle <lea.mick2@gmail.com>
2025-07-26 20:13:11 +0200

    added background - moving eyes

---
+++ portefolio/src/Background.tsx
@@ -0,0 +1,11 @@
+import MovingEyes from "./Components/MovingEyes.tsx";
+import "./Background.css"
+
+function Background(){
+
+    return(
+        <MovingEyes />
+    );
+}
+
+export default Background;
---
+++ portefolio/src/types.ts
@@ -0,0 +1,5 @@
+export type AnimationProps = {
+    transformY: number;
+    opacity: number;
+    config?: { duration?: number; tension?: number; friction?: number; };
+};
